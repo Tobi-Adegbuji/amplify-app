116c2e8f85afbf1d85ef383238bbe8bac2f332eb
Tobi <adegbuji_tobi@yahoo.com>
2020-12-29 10:19:24 -0500

    v2 changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Version 2 of amplify app
         </p>
         <a
           className="App-link"
